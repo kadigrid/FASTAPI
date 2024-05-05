639d50f0dde6b429c59db9c79c36609ef7d7c17a
kadigrid <157012967+kadigrid@users.noreply.github.com>
2024-05-05 22:09:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgres://postgres.dxkimsotxrhgaugrdskk:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgres://postgres.dxkimsotxrhgaugrdskk:Aditya@87971@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
